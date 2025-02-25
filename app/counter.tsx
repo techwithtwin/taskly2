728b9f3761cb0ee2a2a3489d67ebe8d8041fb83c
techwithtwin <realwebtechguru@gmail.com>
2025-02-25 11:23:36 +0300

    Adding expo navigation

---
+++ app/counter.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from "expo-router";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+
+export default function CounterScreen() {
+  const router = useRouter();
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => router.push("/idea")}>
+        <Text style={{ textAlign: "center", marginBottom: 16, fontSize: 18 }}>
+          Go to /idea
+        </Text>
+      </TouchableOpacity>
+      <Text style={styles.text}>Counter</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 24,
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,19 @@
 import { StyleSheet, View } from "react-native";
-import { theme } from "./theme";
-import ShoppingListItem from "./components/ShoppingListItem";
+import ShoppingListItem from "../components/ShoppingListItem";
+import { theme } from "../theme";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Link
+        href="/counter"
+        style={{ textAlign: "center", marginBottom: 16, fontSize: 18 }}
+      >
+        Go to Counter
+      </Link>
+      <StatusBar style="dark" />
       <ShoppingListItem name="Coffee" isCompleted />
       <ShoppingListItem name="Tea" />
       <ShoppingListItem name="Sugar" />

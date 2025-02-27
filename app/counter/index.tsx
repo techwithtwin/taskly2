95959cf4a2503c3a8d49a1cf7781b1397dbd43b0
techwithtwin <realwebtechguru@gmail.com>
2025-02-27 12:22:56 +0300

    Creating a counter

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,10 +1,44 @@
+import { Duration, intervalToDuration, isBefore } from "date-fns";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import { useEffect, useState } from "react";
 import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { theme } from "../../theme";
 import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
-import * as Device from "expo-device";
-import * as Notifications from "expo-notifications";
+import { TimeSegment } from "../../components/TimeSegment";
+
+// ToDO: make it dynamic
+const timestamp = Date.now() + 10 * 1000; // 10 seconds from now
+
+type CountdownStatus = {
+  isOverdue: boolean;
+  distance: Duration;
+};
 
 export default function CounterScreen() {
+  const [status, setStatus] = useState<CountdownStatus>({
+    isOverdue: false,
+    distance: {},
+  });
+
+  useEffect(() => {
+    let intevalId = setInterval(() => {
+      const isOverdue = isBefore(timestamp, Date.now());
+      const distance = intervalToDuration(
+        isOverdue
+          ? { start: timestamp, end: Date.now() }
+          : {
+              start: Date.now(),
+              end: timestamp,
+            },
+      );
+      setStatus({ isOverdue, distance });
+    }, 1000);
+
+    return () => {
+      clearInterval(intevalId);
+    };
+  }, []);
   const scheduleNotification = async () => {
     const result = await registerForPushNotificationsAsync();
     console.log(result);
@@ -30,11 +64,17 @@ export default function CounterScreen() {
   };
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        activeOpacity={0.8}
-        style={styles.button}
-        onPress={scheduleNotification}
-      >
+      <Text style={styles.heading}>
+        Thing {status.isOverdue ? "overdue by" : "due in ..."}
+      </Text>
+
+      <View style={styles.row}>
+        <TimeSegment unit="Days" number={status.distance.days ?? 0} />
+        <TimeSegment unit="Hours" number={status.distance.hours ?? 0} />
+        <TimeSegment unit="Minutes" number={status.distance.minutes ?? 0} />
+        <TimeSegment unit="Seconds" number={status.distance.seconds ?? 0} />
+      </View>
+      <TouchableOpacity activeOpacity={0.8} style={styles.button}>
         <Text style={styles.buttonText}>Schedule Notification</Text>
       </TouchableOpacity>
     </View>
@@ -48,6 +88,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#fff",
   },
+  heading: {},
   button: {
     backgroundColor: theme.colorBlack,
     borderRadius: 6,
@@ -58,5 +99,8 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: "bold",
     textTransform: "uppercase",
+  },
+  row: {
+    flexDirection: "row",
   },
 });
